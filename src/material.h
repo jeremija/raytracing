d11ed027a2b8f3ab86d1db3d3ebd6984eea9928f
Jerko Steiner <jerko.steiner@gmail.com>
2017-09-24 18:01:03 -0400

    Add fuzz parameter to metal (end of ch 9)

--- src/material.h
+++ src/material.h
@@ -36,14 +36,15 @@ class lambertian: public material {
 
 class metal: public material {
   public:
-    metal(const vec3& a): albedo(a) {}
+    metal(const vec3& a, float fuzz): albedo(a), fuzz(fuzz < 1 ? fuzz : 1) {}
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
       vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-      scattered = ray(rec.p, reflected);
+      scattered = ray(rec.p, reflected + this->fuzz * random_in_unit_sphere());
       attenuation = albedo;
       return dot(scattered.direction(), rec.normal) > 0;
     }
     vec3 albedo;
+    const float fuzz;
 };
 
 
