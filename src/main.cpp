b1254ec0b4a13f58d975f978bc8c7cd42baddd9f
Jerko Steiner <jerko.steiner@gmail.com>
2017-09-24 17:07:57 -0400

    Tweak shadow gamma + min float (end of ch 7)

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ vec3 random_in_unit_sphere() {
 
 vec3 color(const ray& r, hitable *world) {
   hit_record rec;
-  if (world->hit(r, 0.0, FLT_MAX, rec)) {
+  if (world->hit(r, 0.001, FLT_MAX, rec)) {
     vec3 target = rec.p + rec.normal + random_in_unit_sphere();
     return 0.5 * color(ray(rec.p, target - rec.p), world);
   }
@@ -49,6 +49,7 @@ int main () {
         col += color (r, world);
       }
       col /= float(ns);
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
       int ir = int(255.99 * col[0]);
       int ig = int(255.99 * col[1]);
