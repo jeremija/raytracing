724a16cfcdf72523a83bdc193af154313d9e9a31
Jerko Steiner <jerko.steiner@gmail.com>
2017-09-24 16:18:57 -0400

    Add basic diffusion

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,20 @@
 #include "hitable_list.h"
 #include "sphere.h"
 
+
+vec3 random_in_unit_sphere() {
+  vec3 p;
+  do {
+    p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+  } while (p.squared_length() >= 1.0);
+  return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
   hit_record rec;
   if (world->hit(r, 0.0, FLT_MAX, rec)) {
-    vec3 N = rec.normal;
-    return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * color(ray(rec.p, target - rec.p), world);
   }
   vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5 * (unit_direction.y() + 1.0);
@@ -25,7 +34,7 @@ int main () {
 
   hitable *list[2];
   list[0] = new sphere(vec3(0, 0, -1), 0.5);
-  list[1] = new sphere(vec3(-100,-100.5, -1), 100);
+  list[1] = new sphere(vec3(0,-100.5, -1), 100);
   hitable *world = new hitable_list(list, 2);
   camera cam;
 
