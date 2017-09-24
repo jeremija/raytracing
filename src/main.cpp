4aed6b65b80d67eaf8c0dc9ec4342b25dbb93691
Jerko Steiner <jerko.steiner@gmail.com>
2017-09-24 09:06:07 -0400

    Add vec3 class

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
+#include "vec3.h"
 
 int main () {
-    int nx = 200;
-    int ny = 100;
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    for (int j = ny - 1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
-            std::cout << ir << " " << ig << " " << ib << "\n";
-        }
+  int nx = 200;
+  int ny = 100;
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  for (int j = ny - 1; j >= 0; j--) {
+    for (int i = 0; i < nx; i++) {
+      vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+      int ir = int(255.99 * col[0]);
+      int ig = int(255.99 * col[1]);
+      int ib = int(255.99 * col[2]);
+      std::cout << ir << " " << ig << " " << ib << "\n";
     }
+  }
 }
